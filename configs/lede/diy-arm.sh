6fcd9527a0368d84cb1be94197cb0d32e3bd44d7
bbufang50 <bbufang50@gmail.com>
2025-09-28 16:18:27 +0800

    Update diy-arm.sh

--- configs/lede/diy-arm.sh
+++ configs/lede/diy-arm.sh
@@ -44,6 +44,11 @@ sed -i 's|^PKG_HASH.*|PKG_HASH:=a7d3785fdd46f1b045b1ef49a2a06e595c327f514b5ee8cd
 #sed -i 's|^PKG_VERSION.*|PKG_VERSION:=1.11.15|' feeds/small/sing-box/Makefile
 #sed -i 's|^PKG_HASH.*|PKG_HASH:=97d58dd873d7cf9b5e4b4aca5516568f3b2e6f5c3dbc93241c82fff5e4a609fd|' feeds/small/sing-box/Makefile
 
+# 清理可能导致编译失败的包
+rm -rf feeds/packages/libs/libfido2
+rm -rf feeds/packages/lang/python/python3
+rm -rf feeds/packages/libs/libssh2
+
 # Delete mosdns
 #rm -rf feeds/packages/net/mosdns
 
