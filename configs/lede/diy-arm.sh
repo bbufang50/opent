51fa216a2b87f6522c8b1f33e9362b32ce539a2e
bbufang50 <bbufang50@gmail.com>
2026-03-25 19:19:23 +0800

    Update diy-arm.sh

--- configs/lede/diy-arm.sh
+++ configs/lede/diy-arm.sh
@@ -121,6 +121,13 @@ fetch_repo_dir \
     "luci-app-argone-config" \
     "feeds/kenzo/luci-app-argone-config"
 
+# 固定 small/luci-app-openclash    
+fetch_repo_dir \
+    "https://github.com/kenzok8/small.git" \
+    "74f1c2606f0b47b989a1a68a2b4b000123a2f915" \
+    "luci-app-openclash" \
+    "feeds/small/luci-app-openclash"
+
 # Delete mosdns
 #rm -rf feeds/packages/net/mosdns
 
