e1456ec58e31bbf843c7e19eaea2f538ad126ba2
bbufang50 <bbufang50@gmail.com>
2026-07-04 06:27:12 +0800

    Update diy-arm.sh

--- configs/lede/diy-arm.sh
+++ configs/lede/diy-arm.sh
@@ -57,6 +57,7 @@ rm -rf feeds/passwall_packages/geoview
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/small/hysteria
 rm -rf feeds/small/luci-app-bypass
 rm -rf feeds/small/luci-app-ssr-plus
 rm -rf feeds/packages/lang/golang
